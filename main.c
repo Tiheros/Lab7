8d997089edb23a4de07f1e5b5e240f88d3a1b089
Tiheros <ipz23-i.dyshliuk@nubip.edu.ua>
2024-07-10 00:56:09 +0300

    lab7

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 int main() {
     double x1, y1, r1, x2, y2, r2;
-    printf("Введите координаты и радиусы кругов (x1, y1, r1, x2, y2, r2): ");
+    printf("Введіть координати та радіуси кіл (x1, y1, r1, x2, y2, r2): ");
     scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &r1, &x2, &y2, &r2);
     
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     
     if (distance > r1 + r2 || distance < fabs(r1 - r2)) {
-        printf("Количество точек пересечения: 0\n");
+        printf("Кількість точок перетину: 0\n");
     } else if (distance == 0 && r1 == r2) {
-        printf("Количество точек пересечения: -1\n");
-    } else if (distance == r1 + r2 || distance == fabs(r1 - r2)) {
-        printf("Количество точек пересечения: 1\n");
+        printf("Кількість точок перетину: -1\n");
+    } else if (distance == r1 + r2 || distance == fabs(r1 - р2)) {
+        printf("Кількість точок перетину: 1\n");
     } else {
-        printf("Количество точек пересечения: 2\n");
+        printf("Кількість точок перетину: 2\n");
     }
     
     return 0;
